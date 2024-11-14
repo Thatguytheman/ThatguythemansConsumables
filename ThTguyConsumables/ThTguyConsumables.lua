92e9f7cf733fd6d3f1fed1a7eed422bf210c7bcc
Thatguytheman <130492366+Thatguytheman@users.noreply.github.com>
2024-11-13 21:49:50 -0500

    Ohala + full spritesheet

--- ThTguyConsumables/ThTguyConsumables.lua
+++ ThTguyConsumables/ThTguyConsumables.lua
@@ -23,21 +23,22 @@ thank you to cryptid mod because i copied a bunch of stuff from it to learn
 	Blank : Spawn last used rune
 
 ]]
-
+local IntrestAmt = 0
 
 --Globals
 local startRef = Game.start_run
 function Game:start_run(args)
     startRef(self, args)
     G.GAME.TGTMchangeHandSize = 0
+    IntrestAmt = G.GAME.interest_cap
 end
 
 
 
 SMODS.Atlas{
     key = "runes",
-	px = 73,
-	py = 103,
+	px = 71,
+	py = 95,
 	path = "Runes.png"
 }
 
@@ -46,7 +47,7 @@ SMODS.ConsumableType{
     key = "Runes",
     primary_colour = HEX("480049"),
     secondary_colour = HEX("890062"),
-    collection_rows = { 2, 5 }, -- 3 pages for all runes
+    collection_rows = { 5, 2 }, -- 3 pages for all runes
     shop_rate = 2,
     loc_txt = {
         name = "Runes",
@@ -83,7 +84,9 @@ SMODS.Consumable{
         CONDITION FOR USING
     end,
     use = function(self, card, area, copier)
-        ON USE
+        G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.3,func = function()
+            ON USE
+        return true end }))
     end,
 }
 
@@ -114,7 +117,7 @@ SMODS.Consumable{
         return G.STATE == G.STATES.SELECTING_HAND
     end,
     use = function(self, card, area, copier)
-        G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.1,func = function()
+        G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.3,func = function()
             ease_discard(1)
             
             --G.GAME.TGTMchangeHandSize = G.GAME.TGTMchangeHandSize - 1
@@ -132,7 +135,7 @@ SMODS.Consumable{
 	discovered = true,
     loc_txt = {
         name = "Dagaz",
-        text = {"{C:red}Disable{} boss blind", "{C:attention}+1{} Ante"}
+        text = {"{C:red}Disable{} boss blind", "{C:attention}No reward money from blind{} "}
     },
 	atlas = "runes",
     set = "Runes",
@@ -146,20 +149,27 @@ SMODS.Consumable{
         return (G.STATE == G.STATES.SELECTING_HAND and G.GAME.blind:get_type() == 'Boss')
     end,
     use = function(self, card, area, copier)
-        G.GAME.blind:disable()
-        ease_ante(1)
+        G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.3,func = function()
+            G.GAME.blind:disable()
+            G.GAME.blind.dollars = 0
+            G.GAME.current_round.dollars_to_be_earned = ''
+        return true end }))
     end,
 }
-    
+
+--Othala
 SMODS.Consumable{
     set_ability = function(self, card, initial, delay_sprites)
         card:set_edition("e_negative")
     end,
 	unlocked = true,
 	discovered = true,
+    loc_vars = function(self, info_queue, card)
+		return { vars = {G.GAME and G.GAME.interest_cap or '???'}}
+	end,
     loc_txt = {
         name = "Othala",
-        text = {"{C:yellow}+15 Dollars{}", "{C:red}-2{} Interest cap"}
+        text = {"Earn {C:attention}interest cap * 5{} Dollars {C:inactive}($#1#){}", "{C:red}-20%{} of Interest cap"}
     },
 	atlas = "runes",
     set = "Runes",
@@ -173,8 +183,20 @@ SMODS.Consumable{
         return true
     end,
     use = function(self, card, area, copier)
-        G.GAME.interest_cap = G.GAME.interest_cap - 10
-        ease_dollars(15)
+        G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.3,func = function()
+            ease_dollars(G.GAME.interest_cap)
+            
+            
+            print(G.GAME.interest_cap)
+            if G.GAME.interest_cap > 5 then
+                G.GAME.interest_cap = math.floor((G.GAME.interest_cap/5) * 0.8) * 5
+            end
+
+            print(G.GAME.interest_cap)
+
+
+
+        return true end }))
     end,
 }
 
@@ -185,3 +207,4 @@ function end_round()
   G.hand:change_size(G.GAME.TGTMchangeHandSize)
   G.GAME.TGTMchangeHandSize = 0
 end
+--
